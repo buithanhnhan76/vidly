13f3ca955cf2971be65aa789c98384a839b58ca4
Nhan <nhanbui37@gmail.com>
2021-01-25 11:58:06 +0700

    add conditional render

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -12,7 +12,13 @@ class Movies extends Component {
      };
 
     render() { 
-        return ( 
+        const {length: count} = this.state.movies;
+        if(count === 0) 
+            return <p>There are no movies in the database.</p>
+
+        return (
+            <React.Fragment>
+            <p>Showing {count} movies in the database</p> 
            <table className="table">
                <thead>
                    <tr>
@@ -35,6 +41,7 @@ class Movies extends Component {
                    
                </tbody>
            </table>
+           </React.Fragment>
          );
     }
 }
